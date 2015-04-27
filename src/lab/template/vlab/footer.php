14377c6ca012fa1190cdeb99c07f074c4dc2f971
anuuuanitha <anithab@am.amrita.edu>
2015-04-27 17:26:36 +0530

    Template changed for testing

--- src/lab/template/vlab/footer.php
+++ src/lab/template/vlab/footer.php
@@ -1,3 +1,38 @@
+   </div>
+   <script type="text/javascript">
+ $(function() {
+ 	squarify('#canvasBox');
+	siteResize();
+ });
+ 
+
+	function squarify(element) {
+		if($(document).innerWidth() > $(document).innerHeight()) {
+			var pgValue = ($(document).innerHeight())-122;
+		} else if($(document).innerHeight() > $(document).innerWidth()) {
+			if($(document).innerWidth() < 601) {
+				var pgValue = $(document).innerWidth() - 55;
+			} else {
+				var pgValue = $(document).innerWidth() - ($(".controlHolder").width() + 55);
+			}
+		}
+		$(element).css('width',(pgValue)+'px');
+		$(element+", .controlHolder ul").css('height',(pgValue)+'px');
+	}
+	
+	function siteResize() {
+		var pgWidth = $('#canvasBox').width() + $(".controlHolder").width() + 55;
+		var siteWidth = (pgWidth); //Controls site width
+		$(".main").css('width',(siteWidth)+'px');
+	}
+	
+	window.onload = function() {
+		$('.controlHolder').wrap("<div id='menu'></div>");
+		$("#menu").append('<div class="g198 menuSet"><a href="#"><img src="<?php echo getTempImage('saveIco.gif'); ?>" alt="Save" width="23" height="22" border="0" /></a><a href="#"><img src="<?php echo getTempImage('printIco.gif'); ?>" width="26" height="22" border="0" /></a><a href="#"><img src="<?php echo getTempImage('helpIco.gif'); ?>" alt="Help" width="25" height="22" border="0" /></a><a href="#"><img src="<?php echo getTempImage('zoomIco.gif'); ?>"alt="Fullscreen" width="23" height="22" border="0" /></a></div>');
+	}
+ 
+</script>
+   
     <footer id="tempFooter"><div class="g396 footer">
    <p class="labName">   <?php echo $GLOBALS['comp_name']; ?> </p>  </div>
   <!-- end .g396 -->
@@ -11,5 +46,6 @@
 <?php
 getWorksheet();
 ?>
+<script type="text/javascript" src="js/resp.js"></script>
 </body>
 </html>
--- src/lab/template/vlab/headerold.php
+++ src/lab/template/vlab/headerold.php
@@ -3,13 +3,11 @@
 <head>
  <!-- Enable IE9 Standards mode -->
 <meta http-equiv="X-UA-Compatible" content="IE=9" >
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <title><?php echo $GLOBALS['sim_title']; ?></title>
 <link rel="stylesheet" type="text/css" media="all" href="<?php echo getTempCss('reset.css'); ?>" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php echo getTempCss('grid.css'); ?>" />
 <link href="<?php echo getTempCss('mainstyle.css'); ?>" rel="stylesheet" type="text/css" />
-<link href="<?php echo getTempCss('resp.css'); ?>" rel="stylesheet" type="text/css" />
 <script src="../lib/js/jquery-latest.js"></script>
 <!--[if IE]><script src="js/excanvas.js"></script><![endif]-->
 <?php getIncludes(); ?>
@@ -59,7 +57,7 @@ document.write("<script type=\"text/javascript\" src=\"js/js-webshim/1.9.7/minif
 
 </head>
 <body>
-<div id="layout" class="main">
+<div class="main">
 <header id="silumatorTemp">
   <div class="g99 logo">
   <img src="<?php echo getTempImage('logo.gif'); ?>" alt="" width="89" height="47" /></div>
@@ -68,13 +66,10 @@ document.write("<script type=\"text/javascript\" src=\"js/js-webshim/1.9.7/minif
     <p id="expName"><?php echo $GLOBALS['sim_title']; ?></p>
   </div>
   <!-- end .g495 -->
-
+    <div class="g198 menuSet">
+    <a href="#"><img src="<?php echo getTempImage('saveIco.gif'); ?>" alt="Save" width="23" height="22" border="0" /></a><a href="#"><img src="<?php echo getTempImage('printIco.gif'); ?>" width="26" height="22" border="0" /></a><a href="#"><img src="<?php echo getTempImage('helpIco.gif'); ?>" alt="Help" width="25" height="22" border="0" /></a><a href="#"><img src="<?php echo getTempImage('zoomIco.gif'); ?>" alt="Fullscreen" width="23" height="22" border="0" /></a>
+  </div>
   <!-- end .g198 -->
 <div class="g792 bannerFoot"></div>
     <!-- end .grid_8 -->
 </header><!-- /header -->
-<div class="body">
-<a href="javascript:" id="menuLink" class="menu-link">
-    <!-- Hamburger icon -->
-    <span></span>
-</a>
